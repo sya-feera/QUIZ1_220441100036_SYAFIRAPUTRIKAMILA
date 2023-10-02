1415ae30497e20e5cb47df8ab7ffa673c07467dd
sya-feera <113223505+sya-feera@users.noreply.github.com>
2023-10-02 14:58:03 +0700

    Add files via upload

---
+++ quiz1_pemvis/Quiz1_pemvis.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package quiz1_pemvis;
+
+/**
+ *
+ * @author firak
+ */
+public class Quiz1_pemvis {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
